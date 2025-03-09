532d1ccbac74ab42ec417e9ff7c608af5ca43ceb
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-09 16:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function secondHighest(arr) 
+		var first=-Infinity;
+			var second=-Infinity;
+			for(let i=0;i<arr.length;i++){
+				if(arr[i]>first){
+					second=first;
+		          first=arr[i];		
+					
+				}else if(arr[i]>second && arr[i]<first){
+					second=arr[i];
+				}
+				//infinity means sabse chta element
+			}
+			return second;
+			}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+		Main();
+	
